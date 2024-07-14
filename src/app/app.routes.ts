7b9f6fb55f146334e6fcef693c599a141edca6b0
Bintousarr <camarabintousarr@gmail.com>
2024-07-14 14:09:34 +0200

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+      path: '',
+      loadComponent: () =>
+        import('./main-application/components/home/home.component').then((x) => x.HomeComponent)
+      },
+      {
+        path: '**'
+       
+      }
+];
